5024cb0ffa54b54ee0bafcf6303b519aa8f85671
sankarnarayananmqer143-ship-it <sankarnarayanan.mqer143@gmail.com>
2025-08-13 15:43:55 +0530

    Add files via upload

---
+++ sankar14.py
@@ -0,0 +1,29 @@
+for i in  range(10):
+    print(i,end=' ')
+  
+
+for i in range(101):
+    print(i, end=' ')
+    
+    
+for i in range(50,101):
+    print(i,end=' ')
+   
+    
+for i in range(5,501,5):
+    print(i,end=' ')
+   
+   
+for i in range(100,151,2):
+    print(i,end=' ')
+    
+   
+for i in range(10,0,-1):
+    print(i,end=' ')
+    
+
+for i in range(100,9,-5):
+    print(i,end=' ')
+   
+    
+
